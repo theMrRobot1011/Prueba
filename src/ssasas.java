2d36cbe31c2c54c3120001e6a95fb276cd2929c2
DAM_1 <DAM_1@PB10>
2019-12-13 19:53:43 +0100

    Sum

--- src/ssasas.java
+++ src/ssasas.java
@@ -1,4 +1,16 @@
 
 public class ssasas {
+	public static void main(String[] args ) {
+	
+	
+	}
+	
+		public static int suma(int operando1,int operando2) {
+			int total = 0;
+			total = operando1 + operando2;
+			return total;
+		}
+		
+	
+	}
 
-}
