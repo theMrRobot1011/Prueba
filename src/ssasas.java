d8fd8803b189c1e54add295439ebb5c83d435b3e
DAM_1 <DAM_1@PB10>
2019-12-13 20:10:16 +0100

    Elevar numeros

--- src/ssasas.java
+++ src/ssasas.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class ssasas {
 	public static void main(String[] args ) {
@@ -26,4 +27,16 @@ public class ssasas {
 			total = operando1 * operando2;
 			return total;
 }
+		public static int elev(int operando1,int operando2) {
+			Scanner tec= new Scanner(System.in);
+			int n, base;
+			System.out.println("Ingrese la base: ");
+			base = tec.nextInt();
+			System.out.println("Ingrese el exponente");
+			n = tec.nextInt();
+			int resultado = (int) Math.pow(base, n);
+			System.out.println(base + " elevado a la " + n + " es igual a " + resultado);
+			return operando2;
+			
+		}
 }
